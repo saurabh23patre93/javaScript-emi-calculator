3f78a20cf43723982816d5ffab0001e14bf2bdc9
saurabh23patre93 <63465162+saurabh23patre93@users.noreply.github.com>
2026-06-06 23:44:33 +0530

    Update emi.js

--- src/emi.js
+++ src/emi.js
@@ -19,5 +19,17 @@ function calculateEMI() {
     var emi = (principalAmount * interestRate * Math.pow(1 + interestRate, years)) / (Math.pow(1 + interestRate, years) - 1);
 
     // Display the result
-    document.getElementById("emiResult").innerText = "Your Monthly EMI is: ₹ " + emi.toFixed(2);
+    document.getElementById("emiResult").innerHTML = "Your Monthly EMI is: <span class='badge bg-primary'>₹ " + emi.toFixed(2) + "</span></b> For a loan amount of <span class='badge bg-secondary'>₹ " + principalAmount + "</span> over <span class='badge bg-success'>" + (years/12) + " years</span> at an interest rate of <span class='badge bg-danger'>" + (interestRate*12*100).toFixed(2) + "%</span>.";
 }    
+
+function AmountTextBoxChange() {
+    document.getElementById("rangeAmount").value = document.getElementById("txtAmount").value;
+}
+
+function YearsTextBoxChange() {
+    document.getElementById("rangeYears").value = document.getElementById("txtYears").value;
+}   
+
+function InterestRateTextBoxChange() {
+    document.getElementById("rangeInterestRate").value = document.getElementById("txtInterestRate").value;
+}
